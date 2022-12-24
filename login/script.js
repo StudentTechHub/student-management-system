6072add3f721258397448514bd2b0f2f444d9e37
Neeraj <neerajantil.antil26@gmail.com>
2022-12-24 21:13:46 +0530

    linked Dashboard with login page

--- login/script.js
+++ login/script.js
@@ -3,6 +3,20 @@ const teacher = document.querySelector('.rectangle1');
 const lights = [...document.querySelectorAll('.light')];
 const base = {height: 405, width: 365};
 
+
+// Input Element
+
+function input(name, type, placeholder, required, ...classList){
+  const input = document.createElement('input');
+  input.name=name;
+  input.type=type;
+  input.placeholder=placeholder;
+  input.required=required;
+  input.classList.add(...classList)
+  
+  return input
+}
+
 [teacher, student].forEach(elem => 
   elem.addEventListener('click', e => {
     if(!elem.clicked){       
@@ -20,8 +34,7 @@ const base = {height: 405, width: 365};
         altElem.parentElement.style.zIndex = '5';
         elem.children[2].style.scale = 0;
         elem.children[1].style.scale = .4;
-        altElem.children[2].style.scale = .6;
-        altElem.children[1].style.scale = .6;
+        altElem.children[2].style.scale = altElem.children[1].style.scale = .6;
       })
 
       // Form 
@@ -29,36 +42,18 @@ const base = {height: 405, width: 365};
       const form = document.createElement('form');
       form.classList.add('form');
 
+      form.action = '../Teacher-Dashboard/index.html';
+
+      let input1, input2;
       if(elem===student){
-        const roll = document.createElement('input');
-        roll.type='number';
-        roll.name='roll';
-        roll.placeholder="Roll Number";
-
-        const date = document.createElement('input');
-        date.type='date';
-        date.name='date';
-
-        roll.classList.add('input', 'roll');
-        date.classList.add('input', 'date');
-
-        form.append(roll, date);
+        input1 = input('number', 'roll', 'Roll Number', true, 'input', 'roll');
+        input2 = input('date', 'date', '', true, 'input', 'date');
       }else{
-        const uid = document.createElement('input');
-        uid.type='text';
-        uid.name='uid';
-        uid.placeholder='User ID';
-
-        const password = document.createElement('input');
-        password.type='password';
-        password.name='password';
-        password.placeholder='Password';
-
-        uid.classList.add('input');
-        password.classList.add('input');
-
-        form.append(uid, password);
+        input1 = input('text', 'uid', 'User ID', true, 'input');
+        input2 = input('password', 'password', 'Password', true, 'input');
       }
+
+      form.append(input1, input2);
 
       const submitParent = document.createElement('button');
       submitParent.classList.add('button');
@@ -83,30 +78,26 @@ const base = {height: 405, width: 365};
 
 // for light with cursor
 
-document.addEventListener('mousemove', e => {
+document.addEventListener('mousemove', e => 
   lights.forEach(light => {
     light.style.display = 'block';
-    light.style.left = `${e.clientX - light.parentElement.getBoundingClientRect().left}px`;   // set left
-    light.style.top = `${e.clientY - light.parentElement.getBoundingClientRect().top}px`;     // set top
-
-    // NOTE: the light elements are relatively positioned in order to follow overflow: hidden; therefore we had to subtract the parent element's top and left margins with cursor's X and Y positions respectively.
+    ['left', 'top'].forEach(dimen => light.style[dimen] = `${e[`client${dimen==='left'?'X':'Y'}`] - light.parentElement.getBoundingClientRect()[dimen]}px`)
   })
-})
+
+  // NOTE: the light elements are relatively positioned in order to follow overflow: hidden; therefore we had to subtract the parent element's top and left margins with cursor's X and Y positions respectively.
+)
 
 
 // Autofill
 
 const params = new URL(window.location).searchParams;
-if(params.get('user')==='teacher'){
-  teacher.click();
+if(params.get('user')==='teacher' && !teacher.click())
   ['uid', 'password'].forEach(param => {
     const elem = [...teacher.querySelectorAll('*')].filter(elem => elem.name===param)[0];      // got the element
     elem.value=params.get(param) ?? '';                                                        // set value
   })
-}else if(params.get('user')==='student'){
-  student.click();
+else if(params.get('user')==='student' && !student.click())
   ['roll', 'date'].forEach(param => {
     const elem = [...student.querySelectorAll('*')].filter(elem => elem.name===param)[0];      // got the element
-    elem.value=(param==='roll'?params.get(param)??'':params.get('date')?params.get('date').slice(4, 8)+'-'+params.get('date').slice(2, 4)+'-'+params.get('date').slice(0, 2):'');           // set value
+    elem.value=(param==='roll'?params.get(param)??'':params.get('date')?`${params.get('date').slice(4, 8)}-${params.get('date').slice(2, 4)}-${params.get('date').slice(0, 2)}`:'');   // set value
   })
-};
